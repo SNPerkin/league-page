8c527b6400eca5feb08873cc044ae87335b21f8e
Samuel Perkin <snperkin@gmail.com>
2023-08-05 22:24:24 +0000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -23,7 +23,8 @@ export const managers = [
        "photo": "/managers/samuel.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2023, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-       "mode": "Grow", // (optional) 'Win Now', 'Dynasty', 'Grow', 'Tank', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+       "mode": 'Frisy', //'Super Team' 'Contender' 'Frisky' 'Fraud' 'Rebuilding' 'Trust The Process'
+       //"mode": "Grow", // (optional) 'Win Now', 'Dynasty', 'Grow', 'Tank', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Tom", // Can be anything (usually your rival's name)
          link: 2, // manager array number within this array, or null to link back to all managers page
